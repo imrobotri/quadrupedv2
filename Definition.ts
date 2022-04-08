c8ab8d4e49e935ef7612b5817a95628e2be153f7
智能移动机器人（中山）研究院 <87523997+imrobotri@users.noreply.github.com>
2022-04-08 13:47:41 +0800

    更新 Definition.ts

--- Definition.ts
+++ Definition.ts
@@ -12,3 +12,21 @@ let rc_att_cmd = 0.00 //Heading
 let robot_mode = 0
 let robot_mode_1 = 0
 let state = 0
+
+//########SPI
+let SSLen = 50
+let InfoTemp = pins.createBuffer(SSLen)
+let ToSlaveBuf = pins.createBuffer(SSLen)
+let SfoCnt = 0
+let DaHeader = 0x2B
+let DaTail = 0xEE
+let usb_send_cnt = 0
+let cnt = 0
+
+//########Steering gear||舵机
+let ToSlaveBuf_1 = pins.createBuffer(SSLen)
+let InfoTemp_1 = pins.createBuffer(SSLen)
+let usb_send_cnt_1 = 0
+let SfoCnt_1 = 0
+let DaHeader_1 = 0x2B
+let DaTail_1 = 0xEE
