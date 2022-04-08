7f9e7148d9b88bf591a072715602a766be5385d6
智能移动机器人（中山）研究院 <87523997+imrobotri@users.noreply.github.com>
2022-04-08 11:43:35 +0800

    更新 Definition.ts

--- Definition.ts
+++ Definition.ts
@@ -1,1 +1,14 @@
-//111111111111
+//########data
+let data_tx = pins.createBuffer(38);
+let gait_mode = 0; //robot status
+let rc_spd_cmd_X = 0.00 //x_speed
+let rc_spd_cmd_y = 0.00 //y_speed
+let rc_att_rate_cmd = 0.00 // Turn to speed
+let rc_spd_cmd_z = 0.00 //mobile speed
+let rc_pos_cmd = 0.00 //height
+let rc_att_cmd_x = 0.00 //Pitch
+let rc_att_cmd_y = 0.00 //Side swing
+let rc_att_cmd = 0.00 //Heading
+let robot_mode = 0
+let robot_mode_1 = 0
+let state = 0
