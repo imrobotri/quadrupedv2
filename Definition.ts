d9773e40f0aa06161e0c73740d6ef15da349701b
智能移动机器人（中山）研究院 <87523997+imrobotri@users.noreply.github.com>
2022-04-27 11:28:26 +0800

    Update Definition.ts

--- Definition.ts
+++ Definition.ts
@@ -381,6 +381,7 @@ function Identify_receive() {
 	    //return 2
     }
    else if (Identify_RX[0] == 0x01 && Identify_RX[1] == 0x10) {
+	basic.showNumber(2)   
 	length_r = Identify_RX[2]
         usMBCRC16(Identify_RX, length_r + 3)
 	if (Identify_RX[length_r + 3] == CRC_H && Identify_RX[length_r + 4] == CRC_L) {   
