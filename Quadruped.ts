5859811016f736731df26354d59516709d8fb393
智能移动机器人（中山）研究院 <87523997+imrobotri@users.noreply.github.com>
2022-04-27 11:09:50 +0800

    Update Quadruped.ts

--- Quadruped.ts
+++ Quadruped.ts
@@ -725,11 +725,11 @@ namespace Quadruped {
         Function_c = 0x0A
         Function_s = 1
 	let i =0 
-        Identify_send()
+        //Identify_send()
         //Identify_receive()
 	while(1){
 		Identify_send()
-		if(Identify_receive() == 2 || (i++)<10)
+		if(Identify_receive() == 2 || (i++)>10)
 			break
 	}    
         return Color_ID;
